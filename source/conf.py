b5db976b44940716020a9b1d5b74e5b45402f9b9
Ohad Manor <omanor@gmail.com>
2015-06-08 14:17:43 -0700

    First commit

--- source/conf.py
+++ source/conf.py
@@ -111,9 +111,9 @@ html_theme = 'better'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {
-#    'sidebarwidth': '300'
-#}
+html_theme_options = {
+    'cssfiles': ['_static/style.css'],
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
