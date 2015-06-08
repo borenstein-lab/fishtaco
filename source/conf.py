52e93084d0dcec46975909fbd9461fd5e62c094b
Ohad Manor <omanor@gmail.com>
2015-06-08 14:49:20 -0700

    First commit

--- source/conf.py
+++ source/conf.py
@@ -131,7 +131,7 @@ html_short_title = "Home"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = '_static/fishtaco_title.png'
+html_logo = '_static/fishtaco_title.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -170,7 +170,7 @@ html_sidebars = {
 #html_domain_indices = True
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
