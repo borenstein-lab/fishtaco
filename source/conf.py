5fc7c59a4c92afde3ae63591793454f23c0603f6
Ohad Manor <omanor@gmail.com>
2015-06-08 13:58:29 -0700

    First commit

--- source/conf.py
+++ source/conf.py
@@ -103,7 +103,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'agogo'
+html_theme = 'nature'
 #sys.path.append(os.path.abspath('_themes'))
 #html_theme_path = ['_themes']
 #html_theme = 'sphinx-theme-okfn'
