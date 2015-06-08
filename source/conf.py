df644364ddc2698d2f5cdfac7f406ac17453538d
Ohad Manor <omanor@gmail.com>
2015-06-08 13:50:48 -0700

    First commit

--- source/conf.py
+++ source/conf.py
@@ -108,6 +108,7 @@ sys.path.append(os.path.abspath('_themes'))
 html_theme_path = ['_themes']
 html_theme = 'sphinx-theme-okfn'
 html_theme_options = {
+        'show_okfn_logo': '_static/fish.jpg',
         'logo_icon': '_static/fish.jpg',
         'git_repo': 'https://github.com/omanor/fishtaco'
 }
